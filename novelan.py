198902ce70655fb7e73d476fe80fd5bf7109ded6
Eric Fournié <efournie@gmail.com>
2022-11-22 09:38:56 +0100

    Wait until the page is loaded instead of fixed delay

--- novelan.py
+++ novelan.py
@@ -4,27 +4,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-import time
+
+# Settings
+debug = True
+heat_pump_ip = '192.168.1.44'
 
 driver = webdriver.Firefox()
-driver.get("http://192.168.1.44/Webserver/index.html")
-assert "Heatpump" in driver.title
+driver.get(f'http://{heat_pump_ip}/Webserver/index.html')
 
-elem = driver.find_element(By.ID, "password_prompt_input")
+assert 'Heatpump' in driver.title
+
+# Default password is empty, just send return 
+elem = driver.find_element(By.ID, 'password_prompt_input')
 elem.clear()
 elem.send_keys(Keys.RETURN)
 
-time.sleep(1)
-
+# Wait until page is loaded, then navigate to temperatures
+element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'Navigation')))
 menu = driver.find_element_by_id('Navigation')
 ActionChains(driver).move_to_element(menu).perform()
-
 info = driver.find_element_by_xpath("//ul[@class='nav']/li/a")
 ActionChains(driver).move_to_element(info).perform()
 
+# Get list of menu entries, then navigate to the first one ("Temperaturen")
 menu_elements = driver.find_elements_by_xpath("//ul[@class='nav']/li/ul/li")
 ActionChains(driver).move_to_element(menu_elements[0]).click().perform()
-# ActionChains(driver).move_to_element(menu).move_to_element(info).move_to_element(menu_elements[0]).click().perform()
-
 
 driver.close()
