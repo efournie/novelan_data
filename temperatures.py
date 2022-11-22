44231c8d367d4b34ea8bf1b6f7f36ca2baaf82a7
Eric Fournié <efournie@gmail.com>
2022-11-22 15:41:34 +0100

    Descriptions modified

--- temperatures.py
+++ temperatures.py
@@ -20,17 +20,17 @@ class Temperature:
 class Temperatures():
     '''Main temperatures class'''
     def __init__(self, temps):
-        self.t_in = Temperature('Incoming temperature', temps[0])
-        self.t_out = Temperature('Outgoing temperature', temps[1])
-        self.t_out_exp = Temperature('Expected outgoing temperature', temps[2])
-        self.t_out_ext = Temperature('Outside outgoing temperature', temps[3])
+        self.t_in = Temperature('Flow temperature', temps[0])
+        self.t_out = Temperature('Return temperature', temps[1])
+        self.t_out_exp = Temperature('Expected return temperature', temps[2])
+        self.t_out_ext = Temperature('Outside return temperature', temps[3])
         self.hot_gas = Temperature('Hot gas', temps[4])
         self.t_ext = Temperature('Outside temperature', temps[5])
         self.mid_temp = Temperature('Average temperature', temps[6])
         self.t_water = Temperature('Hot water temperature', temps[7])
         self.t_water_exp = Temperature('Expected hot wated temperature', temps[8])
         self.heat_source_in = Temperature('Heat source incoming temperature', temps[9])
-        self.t_in_max = Temperature('Maximum input temperature', temps[10])
+        self.t_in_max = Temperature('Maximum flow temperature', temps[10])
         self.overheat = Temperature('Overheat', temps[12])
         self.overheat_exp = Temperature('Expected overheat', temps[13])
         self.t_condensation = Temperature('Condensation temperature', temps[18])
