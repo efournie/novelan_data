ff08311511029a4d1a8bffff9fdd896134c70a36
Eric Fournié <efournie@gmail.com>
2022-11-23 11:52:13 +0100

    Use Chrome webdriver instead of Firefox, better performance on Raspberry Pi

--- novelan.py
+++ novelan.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -28,7 +28,7 @@ def main():
         log('Initialization...')
     opts = Options()
     opts.headless = True
-    driver = webdriver.Firefox(options=opts)
+    driver = webdriver.Chrome(options=opts)
     driver.get(f'http://{args.ip_address}/Webserver/index.html')
     _ = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//html/body')))
     assert 'Heatpump' in driver.title
