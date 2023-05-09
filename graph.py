621bb50b0119bdc7a862e45b30b2637e5fbf9241
Eric Fournié <efournie@gmail.com>
2023-05-09 13:05:38 +0200

    Graph can plot 2 curves on the same graph

--- graph.py
+++ graph.py
@@ -5,10 +5,16 @@ import numpy as np
 import os
 
 class Graph:
-    def __init__(self, filename):
+    def __init__(self, filename, filename2=''):
         self.filename = filename
+        self.filename2 = filename2
         self.timestamps = []
         self.values = []
+        self.timestamps2 = []
+        self.values2 = []
+        self.double = False
+        self.name1 = os.path.basename(filename).split('.')[0]
+        self.name2 = os.path.basename(filename2).split('.')[0]
         # load "database" from filename
         if os.path.exists(self.filename):
             with open(self.filename, 'r') as f:
@@ -18,6 +24,15 @@ class Graph:
                     value = float(l.split(' = ')[1].replace('\n', ''))
                     self.timestamps.append(timestamp)
                     self.values.append(value)
+        if filename2 != '' and os.path.exists(self.filename2):
+            with open(self.filename2, 'r') as f:
+                lines = f.readlines()
+                for l in lines:
+                    timestamp = datetime.strptime(l.split(' = ')[0], '%Y-%m-%d %H:%M:%S')
+                    value = float(l.split(' = ')[1].replace('\n', ''))
+                    self.timestamps2.append(timestamp)
+                    self.values2.append(value)
+            self.double = True
 
     def graph(self, img_filename=''):
         '''Plot a graph of the energy consumption between all measurement times'''
@@ -25,8 +40,11 @@ class Graph:
         ts = []
         filt_len = 11
         plt.figure(figsize=(16,6))
-        plt.plot(self.timestamps, self.values)
+        plt.plot(self.timestamps, self.values, 'b', label=self.name1)
+        if self.double:
+            plt.plot(self.timestamps2, self.values2, 'r', label=self.name2)
         plt.grid(True, 'both', 'y')
+        plt.legend()
         if img_filename == '':
             plt.show()
         else:
@@ -34,11 +52,12 @@ class Graph:
 
 def main():
     # Options
-    parser = argparse.ArgumentParser(description='Read energy usage of a Novelan heat pump')
-    parser.add_argument('-f', '--history_file', type=str, help='Text file where the energy usage will be stored')
-    parser.add_argument('-g', '--graph', type=str, default='', help='Generate a bar plot from all the saved values and save it to this file')
+    parser = argparse.ArgumentParser(description='Generate a graph from a file containing timestamps and values')
+    parser.add_argument('-f', '--history_file', type=str, help='File where the timestamps and values are stored')
+    parser.add_argument('--f2', type=str, default='', help='Second values file (optional)')
+    parser.add_argument('-g', '--graph', type=str, default='', help='Generate a graph from the saved values and save it to this file')
     args = parser.parse_args()
-    e = Graph(args.history_file)
+    e = Graph(args.history_file, args.f2)
     if args.graph != '':
         e.graph(args.graph)
 
